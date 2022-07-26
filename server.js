81ced5d725b3e2db4f7da95fc94cc277cbcefaad
Austin Gentz <austingentz@icloud.com>
2022-07-26 15:42:42 -0700

    able to deploy the server using node.

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const path = require('path');
-const api = require('./public/assets/js/index.js');
+// const api = require('./public/assets/js/index.js');
 
 const PORT = 3001;
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
+// app.use('/api', api);
 
 app.use(express.static('public'));
 // GET Route for homepage
