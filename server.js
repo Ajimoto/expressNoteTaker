23425a2aac21a34b0cf6540f9bf75f7952f6920f
Austin Gentz <austingentz@icloud.com>
2022-08-24 15:56:30 -0700

    added the post route and updated the fs require

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const path = require('path');
-// const api = require('./public/assets/js/index.js');
+const fs = require('fs');
+
+const noteData = require('./db/db.json');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use('/api', api);
 
 app.use(express.static('public'));
 // GET Route for homepage
@@ -19,6 +20,32 @@ app.get('/home', (req, res) =>
 app.get('/notes', (req, res) =>
 	res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+// GET api notes
+app.get('/api/notes', (req, res) =>
+	res.sendFile(path.join(__dirname, 'db/db.json'))
+);
+// POST new note
+app.post('/api/notes', (req, res) => {
+	fs.readFile('./db/db.json', 'utf8', (err, data) => {
+		if (err) {
+			throw err;
+		}
+		const notes = JSON.parse(data);
+		const newNote = {
+			title: req.body.title,
+			text: req.body.text,
+			id: notes.length + 1,
+		};
+		notes.push(newNote);
+		console.log(notes);
+		fs.writeFile('./db/db.json', JSON.stringify(notes), (err) =>
+			console.log(err)
+		);
+	});
+	res.json({ ok: true });
+});
+
+// port data
 app.listen(PORT, () =>
 	console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
